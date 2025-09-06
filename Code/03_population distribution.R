021cb57fa36a9b9be955f43bfb1a801f35eb8851
giovannipremoli <gio32.premoli@gmail.com>
2025-09-06 23:01:28 +0200

    Create 03_population distribution.R

---
+++ Code/03_population distribution.R
@@ -0,0 +1,65 @@
+# Why populations disperse over the landscape in a certain manner?
+
+install.packages("sdm") # stands for species distribution models 
+library(sdm)
+
+install.packages("terra")
+library(terra)
+
+# finds names of files in package with the function and then make that an object (file)
+
+file <- system.file("external/species.shp", package="sdm") # / means that we want the file "species" in the folder "external"
+# with the last part of the command we're expliciting that we want the external from the package sdm since external is in all the folders 
+file
+
+# [1] C:/Users/giovanni/AppData/Local/R/win-library/4.4/sdm/external/species.shp" ????
+
+# using vect() function to make it readable for R
+rana <- vect(file) # we obtain a series of info class spatvect, that is an object from terra that shows vectorial spatial data as points, vectors or polygones 
+
+# to select only the data for the presence of the species (occurrance = 1) 
+rana$Occurrence
+plot(rana)
+
+# we're extracting from rana only the data we're looking for 
+pres <- rana[rana$Occurrence==1] # == means equal while != not equal
+
+par(mfrow=c(1,2))
+plot(rana)
+plot(pres)
+
+# exercise: select data from rana with only absences
+# for absences there's more uncertainity cause you're not sure if the animal in not there, maybe you only haven't see it 
+abse <- rana[rana$Occurrence==0]
+plot(abse)
+
+# exercise: plot in a multiframe presence beside absences 
+par(mfrow=c(1,2))
+plot(pres)
+plot(abse)
+
+# exercise: presence on top of absences
+par(mfrow=c(2,1))
+plot(pres)
+plot(abse)
+
+# exercise: plot presences in blue together with absences in red 
+plot(pres, col="blue")  
+points(abse, col="red") # use points to show absences
+
+# Covariates: datas in raster form to understand ecological parameters 
+elev <- system.file("external/elevation.asc", package="sdm") # .asc cause it refers to raster (images)
+elev
+
+# now we want to see the map of elevation
+elevmap <- rast(elev)
+elevmap
+plot(elevmap)
+
+# exercise: change the colors of the elevation map by the colorRampPalette function
+cl <- colorRampPalette(c("blue", "orange", "purple4"))(100)
+
+# exercise: plot the presences together with elevation map
+plot(elevmap, col=cl)
+points(pres)
+                      
