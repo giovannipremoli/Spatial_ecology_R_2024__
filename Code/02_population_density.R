afe60b60d58bd76e32cf190b954a6a5f15d9beac
giovannipremoli <gio32.premoli@gmail.com>
2025-09-06 22:01:02 +0200

    02_population_density.R

--- Code/02_population_density.R
+++ Code/02_population_density.R
@@ -1,10 +1,10 @@
 # How to calculate the density of individuals in a population?
 
 # installing the spatstat package, needed for point pattern analysis
-install.packages("spatstat")
+install.packages("spatstat") # spatstat stands for "spatial statistics"
 
 # recalling the package
-library(spatstat)
+library(spatstat) 
 
 # dataset
 bei
@@ -16,7 +16,7 @@ plot(bei, pch=19)
 # changing the dimension
 plot(bei, pch=19, cex=0.5)
 
-# additional datasets
+# additional dataset for covariates soil types, temperatures, etc
 bei.extra
 plot(bei.extra)
 
@@ -24,16 +24,16 @@ plot(bei.extra)
 elevation <- bei.extra$elev
 plot(elevation)
 
-# another method to select elements 
+# another method to select elements, in this case we're selecting the first element
 elevation2 <- bei.extra[[1]]
 plot(elevation2)
 
-# density map starting from points to a continuous surface 
+# density map of a population, starting from points  
 density(bei)
 densitymap <- density(bei)
 plot(densitymap)
 
-points(bei) # add the points 
+points(bei) # add the points of the dataset, we're showing together the map of density and the original points  
 points(bei, col="green", pch=19, cex=0.5) # assign color, dimension and symbol
 
 # build a multiframe
@@ -41,24 +41,28 @@ par(mfrow=c(1,2)) # 1 row 2 columns
 plot(elevation)
 plot(densitymap)
 
+# multiframe with 2 rows and 1 column
 par(mfrow=c(2,1))
 plot(elevation)
 plot(densitymap)
 
-# back to the original plot 
+# deleting the multiframe  
 dev.off()
 
-# Changing colors to maps
-cln <- colorRampPalette(c("red", "orange", "yellow"))(15)
-plot(densitymap, col=cln)
+# changing colors to maps
+cl1 <- colorRampPalette(c("red", "orange", "yellow"))(3) # 3 is for gradients
+plot(densitymap, col=cl1)
 
-# where we can find all the colours available in R
-http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
+# if we want to increase the ammount of gradients
+cl1 <- colorRampPalette(c("red", "orange", "yellow"))(100)
 
-clg <- colorRampPalette(c("darkmagenra", "lightgrey", "forestgreen"))(100)
-plot(densitymap, col=clg)
+# where we can find all the colours available in R "http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf"
 
-# esercizio per mettere le due mappe di due colori diversi una accanto all'altra
+# different colours, more gradients
+cl2 <- colorRampPalette(c("darkmagenta", "lightgrey", "forestgreen"))(100)
+plot(densitymap, col=cl2)
+
+# excercise: build a multiframe and plot two different densitymaps with two different palettes
 par(mfrow=c(1,2))
-plot(densitymap, col=cln)
-plot(densitymap, col=clg)
+plot(densitymap, col=cl1)
+plot(densitymap, col=cl2)
