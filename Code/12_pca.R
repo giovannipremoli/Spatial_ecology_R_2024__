24027e639ca7c7fc10db3a8e1a57d215890661ef
giovannipremoli <gio32.premoli@gmail.com>
2025-09-08 17:26:27 +0200

    12_pca.R

---
+++ Code/12_pca.R
@@ -0,0 +1,22 @@
+# PCA
+
+library(imageRy)
+library(terra)
+im.list()
+
+sent <- im.import("sentinel.png")
+
+# to see the correlation between all the bands 
+pairs(sent)
+
+# perform PCA on sent
+sentpc <- im.pca(sent)
+# gave us 4 values of sd for each band
+
+# extract pc1 that has more variability so more info
+pc1 <- sentpc[[1]] #88,59% of variability
+pc1
+
+# calculate sd on top of pc1
+pc1sd <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
+plot(pc1sd)
