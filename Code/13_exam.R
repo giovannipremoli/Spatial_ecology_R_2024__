8a832f56a0490205bd14e5e8fcdbefe473e98fde
giovannipremoli <gio32.premoli@gmail.com>
2026-01-19 21:37:22 +0100

    13_exam.R

---
+++ Code/13_exam.R
@@ -0,0 +1,4 @@
+# Multitemporal analysis of vegetation health and landscape variability in the Blue Mountains National Park (Australia) due to the bushfires of 2019-2020.
+
+# Installing the packages necessary for the analysis. The function is install.packages(). 
+install
