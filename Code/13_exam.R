5f15f06af01c65f2b6bdfbe8ab580db058c9c18c
giovannipremoli <gio32.premoli@gmail.com>
2026-01-19 21:44:01 +0100

    Update 13_exam.R

--- Code/13_exam.R
+++ Code/13_exam.R
@@ -1,4 +1,10 @@
 # Multitemporal analysis of vegetation health and landscape variability in the Blue Mountains National Park (Australia) due to the bushfires of 2019-2020.
 
 # Installing the packages necessary for the analysis. The function is install.packages(). 
-install
+install.packages("terra") 
+install.packages("ggplot2")
+install.packages("patchwork")
+
+# Installing "imageRy" package from GitHub
+install.github("ducciorocchini/imageRy")
+
