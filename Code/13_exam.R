3f3c4e2d89df3d1270d7f6e8a404026d41692983
giovannipremoli <gio32.premoli@gmail.com>
2026-01-19 22:03:42 +0100

    Update 13_exam.R

--- Code/13_exam.R
+++ Code/13_exam.R
@@ -1,9 +1,10 @@
 # Multitemporal analysis of vegetation health and landscape variability in the Blue Mountains National Park (Australia) due to the bushfires of 2019-2020.
 
 # Installing the packages necessary for the analysis. The function is install.packages(). 
-install.packages("terra") 
-install.packages("ggplot2")
-install.packages("patchwork")
+install.packages("terra") # Used for handling spatial data, particularly raster and vector data (e.g. satellite images).
+install.packages("ggplot2") # Used for creating statistical graphs. 
+install.packages("patchwork") # Used for combining multiple plots in a single window for comparisons.
+install.packages("viridis") # Used for colorblind-friendly and uniform color palettes
 
 # Installing "imageRy" package from GitHub
 install.github("ducciorocchini/imageRy")
