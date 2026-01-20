62eaf4e38f15549bd4475c7b76946212875aae12
giovannipremoli <gio32.premoli@gmail.com>
2026-01-20 12:13:50 +0100

    Update 13_exam.R

--- Code/13_exam.R
+++ Code/13_exam.R
@@ -26,7 +26,50 @@ setwd("/Users/giovannipremoli/Desktop/spatial ecology in R")
 # Checking if the path is correct. 
 getwd()
 
-# Importing the 2019 Sentinel-2 images for the pre-fire analysis. TC stands for True Colors and FC for False Colors. The images I've chosen are all cloudless.
+# Importing the 2019 Sentinel-2 images for the pre-fire analysis. TC stands for True Colors (bands: red, green and blue)and FC for False Colors (bands: NIR, green, blue). The images I've chosen are all cloudless.
 tc19 <- rast("2019_TC.jpg") # rast() imports the file as a SpatRaster object, preserving its multilayer structure. 
 fc19 <- rast("2019_FC.jpg") 
+
+# Visualizing the two images from 2019.
+par(mfrow=c(2,1))
+plot(tc19)
+plot(fc19) 
+
+dev.off() # Clearing the current graph window.
+
+# Extracting the bands from 2019 images. Useful to classify the objects and extract more information from the data.
+# From the TC image I'm taking the red (r), blue (b) and green (g) bands.
+r19 <- tc19[[1]]
+g19 <- tc19[[2]]
+b19 <- tc19[[3]]
+
+# From the FC image I'm taking the NIR band.
+nir19 <- fc19[[1]] 
+
+# Combining the bands in a single object.
+stack19 <- c(r19, g19, b19, nir19)
+
+# Plotting the result to verify the 4 layers.
+plot(stack19)
+
+# Visualizing the different bands in a RGB plot alternating their color representation. This is useful to highlight specific environmental features.
+im.plotRGB(stack19, r=4, g=2, b=3) # NIR on red. Healthy vegetation appear bright red.
+im.plotRGB(stack19, r=1, g=4, b=3) # NIR on green. Active clorophylle is highlighted in green tones.
+im.plotRGB(stack19, r=1, g=2, b=4) # NIR on blue. Emphazize surface reflectance properties, useful to identify structural differences in the landscape.
+
+# Calculating the Normalized Difference Vegetation Index (NDVI). The aim is to quantify vegetation health by measuirng the difference between NIR (reflected by leaves) and red wavelengths (absorbed by chlorophyll).
+# My goal is to assess forest's health before the fires of late 2019 - early 2020.
+# Formula: (NIR - red)/(NIR + red)
+
+ndvi19 <- (nir19 - r19)/(nir19 + r19)
+
+# Visualizing the NDVI map. Instead of creating a manual palette, I'm using the viridis function to ensure that maps are accesible to colorblind viewers.
+# I'm also assigning a title to the plot with main =.
+plot(ndvi19, col=viridis(100), main="NDVI 2019")
+
+dev.off()
+
+
+
+
 
